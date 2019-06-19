7e1f50faf2cad7ef708ec0e5e91ae556411842f8
Matt Aiken <mattaiken@hotmail.co.uk>
2019-06-19 12:58:08 +0100

    Headline component with tests

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './app.scss';
 
 import Header from './components/header/Header';
+import Headline from './components/headline/Headline';
 
 function App() {
   return (
     <div>
       <Header />
+      <section className="main">
+        <Headline headline="Posts" desc="Click the button to render posts!" />
+      </section>
     </div>
   );
 }
