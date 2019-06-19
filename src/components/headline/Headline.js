d158c363a1f8a1e3a78823af0b1af97f1151f9fc
Matt Aiken <mattaiken@hotmail.co.uk>
2019-06-19 14:40:57 +0100

    PropTypes added to Headline component with tests

--- src/components/headline/Headline.js
+++ src/components/headline/Headline.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
 
 class Headline extends Component {
   constructor(props) {
@@ -14,11 +16,27 @@ class Headline extends Component {
 
     return (
       <div data-test="headlineComponent">
-        <h1 data-test="headline">{headline}</h1>
+        <h1 className="headline" data-test="headline">
+          {headline}
+        </h1>
         <p data-test="description">{desc}</p>
       </div>
     );
   }
 }
+
+Headline.propTypes = {
+  headline: PropTypes.string,
+  desc: PropTypes.string,
+  tempArr: PropTypes.arrayOf(
+    PropTypes.shape({
+      fName: PropTypes.string,
+      lName: PropTypes.string,
+      email: PropTypes.string,
+      age: PropTypes.number,
+      onlineStatus: PropTypes.bool
+    })
+  )
+};
 
 export default Headline;
